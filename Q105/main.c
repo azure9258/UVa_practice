52b3c3259fb14bd2f94f93d727dd3615cf53447d
azure9258 <azure9258@gmail.com>
2014-05-08 23:41:27 +0800

    modify and finish Q105/main.c

--- Q105/main.c
+++ Q105/main.c
@@ -1,20 +1,56 @@
 /*
-method : 
-Last modified : 2014/05/
+method : brute-force (save all the height of each x-coordinate)
+Last modified : 2014/05/08
 */
 
 #include <stdio.h>
 
 int main()
 {
-    int i, j, k, x, y, z; /* for loop */
+    int buildings[5000][3]; /* [0]:left, [1]:height, [2]:right */
+    int i, j; /* for loop */
+    int x_coordinate[10000] = {0};
+    int counter = 0;
+    int rightmost = 0;
+    int temp;
 
-    /* freopen("input.txt", "r", stdin); */
+    /*freopen("input.txt", "r", stdin);*/
 
-    while(scanf("%d", &n) == 1)
+    while(scanf("%d %d %d", &buildings[counter][0], &buildings[counter][1], &buildings[counter][2]) == 3)
     {
-        
+        counter++;
     }
+
+    for(i=0;i<counter;i++)
+    {
+        for(j=buildings[i][0];j<=buildings[i][2]-1;j++)
+        {
+            if(x_coordinate[j] < buildings[i][1])
+            {
+                x_coordinate[j] = buildings[i][1];
+            }
+        }
+        if(rightmost < buildings[i][2])
+        {
+            rightmost = buildings[i][2];
+        }
+    }
+
+    temp = buildings[0][0];
+    printf("%d %d", temp, x_coordinate[temp]);
+
+    for(i=buildings[0][0]+1;i<rightmost;i++)
+    {
+        if(x_coordinate[i] == x_coordinate[temp])
+        {
+            continue;
+        }
+        temp = i;
+        printf(" %d %d", i, x_coordinate[i]);
+
+    }
+    printf(" %d 0\n",rightmost);
+
 
     return 0;
 }
