edc04a522902e6bfb63d894d89f7be4394ab0d33
azure9258 <azure9258@gmail.com>
2014-05-09 17:45:12 +0800

    add Q106/main.c initial module

---
+++ Q106/main.c
@@ -0,0 +1,21 @@
+/*
+method : 
+Last modified : 2014/05/
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int n; /* input */
+    int i, j; /* for loop */
+
+    /*freopen("input.txt", "r", stdin);*/
+
+    while(scanf("%d", &n) == 1)
+    {
+        
+    }
+
+    return 0;
+}
