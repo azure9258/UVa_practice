6a490a8eb9f419148dfbcf4f0f678dde221bf378
azure9258 <azure9258@gmail.com>
2014-05-10 12:47:36 +0800

    add Q107/main.c initial module

---
+++ Q107/main.c
@@ -0,0 +1,25 @@
+/*
+method : 
+Last modified : 2014/05/10
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n1, n2; /* input */
+    int i, j; /* for loop */
+
+    /* freopen("input.txt", "r", stdin); */
+
+    while(scanf("%d %d", &n1, &n2) == 2)
+    {
+        if(!n1 && !n2)
+		{
+			break;
+		}
+    }
+
+    return 0;
+}
