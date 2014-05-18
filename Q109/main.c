ff9d01f6230a5a186d78b088f72b44b3e5a1ba16
azure9258 <azure9258@gmail.com>
2014-05-18 19:38:18 +0800

    modify and finish Q109/main.c

--- Q109/main.c
+++ Q109/main.c
@@ -1,4 +1,5 @@
 /*
+tip : if no line feed at output, it would get "Wrong answer"
 method : Monotone Chain Convex Hull (reference : http://www.algorithmist.com/index.php/Monotone_Chain_Convex_Hull)
 Last modified : 2014/05/1
 */
@@ -11,6 +12,7 @@ struct point
     int y;
 };
 
+/* for qsort */
 int qsortCompareByxy(const void *a1, const void *a2)
 {
     struct point p1 = *(struct point*) a1;
@@ -22,21 +24,80 @@ int qsortCompareByxy(const void *a1, const void *a2)
     return p1.x - p2.x;
 }
 
+/* calcute the area of polygon */
+double area(struct point kingdom[], int num_sites)
+{
+    int i; /* for loop */
+    double counter = 0.0;
+    for(i=1;i<num_sites;i++)
+    {
+        counter += kingdom[i-1].x * kingdom[i].y - kingdom[i].x * kingdom[i-1].y;
+    }
+    counter *= 0.5;
+    return counter;
+}
+
+/* check if missile hit inside the kingdom */
+/* method : find the sum of area of missile with all two neighbor site.
+            if equal to area of kingdom, missile is inside the kingdom */
+int missile_hit(int x, int y, struct point kingdom[], int num_sites)
+{
+    int i; /* for loop */
+    double kingdom_area = area(kingdom, num_sites); /* area of kingdom */
+    double counter = 0.0;
+    double temp;
+
+    /* accumulative total */
+    for(i=0;i<num_sites-1;i++)
+    {
+        struct point tri[4]; /* the first one and the last one is duplicated, so 3+1=4 */
+        tri[0].x = tri[3].x = kingdom[i].x;
+        tri[0].y = tri[3].y = kingdom[i].y;
+        tri[1].x = kingdom[i+1].x;
+        tri[1].y = kingdom[i+1].y;
+        tri[2].x = x;
+        tri[2].y = y;
+
+        /* equal to counter += fabs(area(tri, 4)) */
+        temp = area(tri, 4);
+        if(temp < 0)
+        {
+            counter += (-1) * temp;
+        }
+        else
+        {
+            counter += temp;
+        }
+    }
+    if(counter == kingdom_area)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int main()
 {
     int i, j, k, m, n, o, x, y, z; /* for loop */
     int num; /* input */
-    struct point kingdom[20][100], missile[30];
+    struct point kingdom[20][100], missile[30]; /* input data */
     int num_sites[20], num_kingdom = 0, num_missile = 0;
-    struct point hull1[100], hull2[100];
-    int num_hull1, num_hull2;
+    struct point hull1[100], hull2[100]; /* for Monotone Chain Convex Hull algorithm used */
+    int num_hull1, num_hull2; /* number of hull1 and hull2 */
     double temp_y;
-    struct point new_hull[20][100];
+    struct point new_hull[20][100]; /* result of Convex Hull */
     int num_newhull[20];
     int counter;
+    int hit_kingdom[20] = {0}; /* check if kingdom hit or not */
+    int hit_missile[30] = {0};
+    double result = 0.0, temp;
 
-    freopen("input.txt", "r", stdin);
+    /* freopen("input.txt", "r", stdin); */
 
+    /* get input */
     while((scanf("%d", &num) == 1) && (num != -1))
     {
         num_sites[num_kingdom] = num;
@@ -51,11 +112,13 @@ int main()
         num_missile++;
     }
 
+    /* sort by coordinate in order to use Monotone Chain Convex Hull algorithm */
     for(i=0;i<num_kingdom;i++)
     {
         qsort(kingdom[i], num_sites[i], sizeof(struct point), qsortCompareByxy);
     }
 
+    /* run Monotone Chain Convex Hull algorithm */
     for(i=0;i<num_kingdom;i++)
     {
         num_hull1 = 0;
@@ -63,6 +126,11 @@ int main()
         {
             while(num_hull1 >= 2)
             {
+                if(kingdom[i][j].x == hull1[num_hull1-2].x)
+                {
+                    num_hull1--;
+                    continue;
+                }
                 temp_y = (double)hull1[num_hull1-2].y + (double)(hull1[num_hull1-1].x - hull1[num_hull1-2].x) * (double)(kingdom[i][j].y - hull1[num_hull1-2].y) / (double)(kingdom[i][j].x - hull1[num_hull1-2].x);
                 if(temp_y > (double)hull1[num_hull1-1].y)
                 {
@@ -83,6 +151,11 @@ int main()
         {
             while(num_hull2 >= 2)
             {
+                if(kingdom[i][j].x == hull2[num_hull2-2].x)
+                {
+                    num_hull2--;
+                    continue;
+                }
                 temp_y = (double)hull2[num_hull2-2].y + (double)(hull2[num_hull2-1].x - hull2[num_hull2-2].x) * (double)(kingdom[i][j].y - hull2[num_hull2-2].y) / (double)(kingdom[i][j].x - hull2[num_hull2-2].x);
                 if(temp_y < (double)hull2[num_hull2-1].y)
                 {
@@ -93,12 +166,12 @@ int main()
                     num_hull2--;
                 }
             }
-            hull1[num_hull2].x = kingdom[i][j].x;
-            hull1[num_hull2].y = kingdom[i][j].y;
+            hull2[num_hull2].x = kingdom[i][j].x;
+            hull2[num_hull2].y = kingdom[i][j].y;
             num_hull2++;
         }
-        num_hull2--;
 
+        /* save the result to new_hull */
         counter = 0;
         for(k=0;k<num_hull1;k++)
         {
@@ -114,6 +187,48 @@ int main()
         }
         num_newhull[i] = counter;
     }
+
+    /* process the missile */
+    for(i=0;i<num_missile;i++)
+    {
+        for(j=0;j<num_kingdom;j++)
+        {
+            if(hit_missile[i] == 1)
+            {
+                break;
+            }
+            if(hit_kingdom[j] == 1)
+            {
+                continue;
+            }
+            if(missile_hit(missile[i].x, missile[i].y, new_hull[j], num_newhull[j]) == 1)
+            {
+                hit_missile[i] = 1;
+                hit_kingdom[j] = 1;
+            }
+        }
+    }
+
+    /* calculate the sum of all hit kingdom */
+    for(i=0;i<num_kingdom;i++)
+    {
+        if(hit_kingdom[i] == 1)
+        {
+            /* equal to result += fabs(area(new_hull[i], num_newhull[i])) */
+            temp = area(new_hull[i], num_newhull[i]);
+            if(temp < 0)
+            {
+                result += (-1) * temp;
+            }
+            else
+            {
+                result += temp;
+            }
+        }
+    }
+
+    /* output the result */
+    printf("%.2lf\n", result);
 
     return 0;
 }
