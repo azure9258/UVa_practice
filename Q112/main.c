438a42d32655c28fbf65071e06878dd7d1dde702
azure9258 <azure9258@gmail.com>
2014-05-22 22:28:32 +0800

    modify Q112/main.c to add comment

--- Q112/main.c
+++ Q112/main.c
@@ -3,7 +3,8 @@ method : stack
 Last modified : 2014/05/22
 */
 #include <stdio.h>
-#define N 10000
+#define N1 5000
+#define N2 1000
 
 int main()
 {
@@ -11,12 +12,12 @@ int main()
     int n; /* input */
     int level; /* current level */
     char input_c; /* input */
-    char expression[N]; /* save the char into array */
-    int expression_d[N]; /* save the integer part into this array */
+    char expression[N1]; /* save the char into array */
+    int expression_d[N2]; /* save the integer part into this array */
     int num_expression, num_expression_d; /* the number of array */
     int num; /* temporarily save the value of integer */
     int pre_num; /* check if previous char is num */
-    int stack[N];
+    int stack[N2];
     int num_stack;
     int check; /* check yes or no */
     int sum;
@@ -64,15 +65,15 @@ int main()
             }
             if(input_c == '(')
             {
-                level += 1;
+                level++;
                 expression[num_expression++] = '(';
             }
             else if(input_c == ')')
             {
-                level -= 1;
+                level--;
                 expression[num_expression++] = ')';
             }
-            else if(input_c == '-')
+            else if(input_c == '-') /* if number is negative */
             {
                 minus = 1;
             }
@@ -84,11 +85,12 @@ int main()
             }
         }
 
-        num_stack = 0;
+        /* start to calculate all the sums of the paths */
+        num_stack = 0; /* stack is empty */
         level = 1;
-        i = 1;
-        j = 0;
-        check = 0;
+        i = 1; /* index of expression array */
+        j = 0; /* index of expression_d array */
+        check = 0; /* check if the result is yes */
         while(level != 0)
         {
             if(expression[i] == '(')
