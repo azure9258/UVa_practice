45542083fc4321fbf4f48bede8ed60f0efa6a996
azure9258 <azure9258@gmail.com>
2014-05-28 19:30:34 +0800

    modify Q116/main.c to optimize the efficiency and process duplicated code

--- Q116/main.c
+++ Q116/main.c
@@ -1,7 +1,7 @@
 /*
 tip : not accept if space existed at the end of the line
 method : Dynamic Programming
-Last modified : 2014/05/27
+Last modified : 2014/05/28
 */
 #include <stdio.h>
 #define max_m 10
@@ -23,6 +23,7 @@ int main()
     int sumPath[max_m][max_n], sum1, sum2, sum3; /* the sum value of each path */
     int nowPath, nextPath;
     int result, min;
+    int cPath; /* path choice */
 
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -39,7 +40,92 @@ int main()
             }
         }
 
-        /* initialize */
+        /* processing special case */
+        if(m == 1)
+        {
+            min = matrix[0][0];
+            printf("1");
+            for(i=1;i<n;i++)
+            {
+                printf(" 1");
+                min += matrix[0][i];
+            }
+            printf("\n");
+            printf("%d\n", min);
+            continue;
+        }
+        else if(m == 2)
+        {
+            nowPath = 1;
+            nextPath = 0;
+            path[0][0][0] = 0;
+            path[0][1][0] = 1;
+            sumPath[0][0] = matrix[0][0];
+            sumPath[1][0] = matrix[1][0];
+            for(i=1;i<n;i++)
+            {
+                nextPath = nowPath;
+                nowPath = (nextPath+1) % 2;
+                if(sumPath[0][i-1] < sumPath[1][i-1])
+                {
+                    cPath = 1;
+                }
+                else if(sumPath[0][i-1] > sumPath[1][i-1])
+                {
+                    cPath = 2;
+                }
+                else
+                {
+                    cPath = 1;
+                }
+                if(cPath == 1)
+                {
+                    for(k=0;k<i;k++)
+                    {
+                        path[nextPath][0][k] = path[nowPath][0][k];
+                        path[nextPath][1][k] = path[nowPath][0][k];
+                    }
+                    path[nextPath][0][i] = 0;
+                    path[nextPath][1][i] = 1;
+                    sumPath[0][i] = sumPath[0][i-1] + matrix[0][i];
+                    sumPath[1][i] = sumPath[0][i-1] + matrix[1][i];
+                }
+                else
+                {
+                    for(k=0;k<i;k++)
+                    {
+                        path[nextPath][0][k] = path[nowPath][1][k];
+                        path[nextPath][1][k] = path[nowPath][1][k];
+                    }
+                    path[nextPath][0][i] = 0;
+                    path[nextPath][1][i] = 1;
+                    sumPath[0][i] = sumPath[1][i-1] + matrix[0][i];
+                    sumPath[1][i] = sumPath[1][i-1] + matrix[1][i];
+                }
+            }
+            if(sumPath[0][n-1] < sumPath[1][n-1])
+            {
+                result = 0;
+            }
+            else if(sumPath[0][n-1] > sumPath[1][n-1])
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+            printf("%d", path[nextPath][result][0]+1);
+            for(i=1;i<n;i++)
+            {
+                printf(" %d", path[nextPath][result][i]+1);
+            }
+            printf("\n");
+            printf("%d\n", sumPath[result][n-1]);
+            continue;
+        }
+
+        /* if m >= 3 */
         nowPath = 1;
         nextPath = 0;
         for(i=0;i<m;i++)
@@ -61,60 +147,30 @@ int main()
                 {
                     if(sum2 <= sum3) /* sum1 is min */
                     {
-                        for(k=0;k<i;k++)
-                        {
-                            path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                        }
-                        path[nextPath][j][i] = j;
-                        sumPath[j][i] = sum1;
+                        cPath = 1;
                     }
                     else if(sum1 < sum3) /* sum1 is min */
                     {
-                        for(k=0;k<i;k++)
-                        {
-                            path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                        }
-                        path[nextPath][j][i] = j;
-                        sumPath[j][i] = sum1;
+                        cPath = 1;
                     }
                     else if(sum1 > sum3) /* sum3 is min*/
                     {
-                        for(k=0;k<i;k++)
-                        {
-                            path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                        }
-                        path[nextPath][j][i] = j;
-                        sumPath[j][i] = sum3;
+                        cPath = 3;
                     }
                     else /* both sum1 and sum3 is min */
                     {
                         result = lexicographicallyCompare(path[nowPath][(j+m-1)%m], path[nowPath][(j+1)%m], i);
                         if(result == 1) /* sum1 is lexicographically smaller */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum1;
+                            cPath = 1;
                         }
                         else if(result == 2) /* sum3 is lexicographically smaller */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum3;
+                            cPath = 3;
                         }
                         else /* sum1 and sum3 is lexicographically equal */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum1;
+                            cPath = 1;
                         }
                     }
                 }
@@ -122,51 +178,26 @@ int main()
                 {
                     if(sum2 > sum3) /* sum3 is min*/
                     {
-                        for(k=0;k<i;k++)
-                        {
-                            path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                        }
-                        path[nextPath][j][i] = j;
-                        sumPath[j][i] = sum3;
+                        cPath = 3;
                     }
                     else if(sum2 < sum3) /* sum2 is min*/
                     {
-                        for(k=0;k<i;k++)
-                        {
-                            path[nextPath][j][k] = path[nowPath][j][k];
-                        }
-                        path[nextPath][j][i] = j;
-                        sumPath[j][i] = sum2;
+                        cPath = 2;
                     }
                     else /* both sum2 and sum3 is min */
                     {
                         result = lexicographicallyCompare(path[nowPath][j], path[nowPath][(j+1)%m], i);
                         if(result == 1) /* sum2 is lexicographically smaller */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][j][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum2;
+                            cPath = 2;
                         }
                         else if(result == 2) /* sum3 is lexicographically smaller */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum3;
+                            cPath = 3;
                         }
                         else /* sum2 and sum3 is lexicographically equal */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][j][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum2;
+                            cPath = 2;
                         }
                     }
                 }
@@ -174,42 +205,22 @@ int main()
                 {
                     if(sum1 > sum3) /* sum3 is min */
                     {
-                        for(k=0;k<i;k++)
-                        {
-                            path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                        }
-                        path[nextPath][j][i] = j;
-                        sumPath[j][i] = sum3;
+                        cPath = 3;
                     }
                     else if(sum1 < sum3) /* both sum1 and sum2 is min */
                     {
                         result = lexicographicallyCompare(path[nowPath][(j+m-1)%m], path[nowPath][j], i);
                         if(result == 1) /* sum1 is lexicographically smaller */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum1;
+                            cPath = 1;
                         }
                         else if(result == 2) /* sum2 is lexicographically smaller */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][j][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum2;
+                            cPath = 2;
                         }
                         else /* sum1 and sum2 is lexicographically equal */
                         {
-                            for(k=0;k<i;k++)
-                            {
-                                path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                            }
-                            path[nextPath][j][i] = j;
-                            sumPath[j][i] = sum1;
+                            cPath = 1;
                         }
                     }
                     else /* sum1, sum2, sum3 is same*/
@@ -220,52 +231,27 @@ int main()
                             result = lexicographicallyCompare(path[nowPath][j], path[nowPath][(j+1)%m], i);
                             if(result == 1) /* sum1 is lexicographically smallest */
                             {
-                                for(k=0;k<i;k++)
-                                {
-                                    path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                                }
-                                path[nextPath][j][i] = j;
-                                sumPath[j][i] = sum1;
+                                cPath = 1;
                             }
                             else if(result == 2) /* both sum1 and sum3 is lexicographically smaller than sum2 */
                             {
                                 result = lexicographicallyCompare(path[nowPath][(j+m-1)%m], path[nowPath][(j+1)%m], i);
                                 if(result == 1) /* sum1 is lexicographically smallest */
                                 {
-                                    for(k=0;k<i;k++)
-                                    {
-                                        path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                                    }
-                                    path[nextPath][j][i] = j;
-                                    sumPath[j][i] = sum1;
+                                    cPath = 1;
                                 }
                                 else if(result == 2) /* sum3 is lexicographically smallest */
                                 {
-                                    for(k=0;k<i;k++)
-                                    {
-                                        path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                                    }
-                                    path[nextPath][j][i] = j;
-                                    sumPath[j][i] = sum3;
+                                    cPath = 3;
                                 }
                                 else /* both sum1 and sum3 is the lexicographically smallest */
                                 {
-                                    for(k=0;k<i;k++)
-                                    {
-                                        path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                                    }
-                                    path[nextPath][j][i] = j;
-                                    sumPath[j][i] = sum1;
+                                    cPath = 1;
                                 }
                             }
                             else /* sum1 is lexicographically smallest */
                             {
-                                for(k=0;k<i;k++)
-                                {
-                                    path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                                }
-                                path[nextPath][j][i] = j;
-                                sumPath[j][i] = sum1;
+                                cPath = 1;
                             }
                         }
                         else if(result == 2) /* sum2 is lexicographically smaller than sum1 */
@@ -273,52 +259,27 @@ int main()
                             result = lexicographicallyCompare(path[nowPath][(j+m-1)%m], path[nowPath][(j+1)%m], i);
                             if(result == 1) /* sum2 is lexicographically smallest */
                             {
-                                for(k=0;k<i;k++)
-                                {
-                                    path[nextPath][j][k] = path[nowPath][j][k];
-                                }
-                                path[nextPath][j][i] = j;
-                                sumPath[j][i] = sum2;
+                                cPath = 2;
                             }
                             else if(result == 2) /* both sum2 and sum3 is lexicographically smaller than sum1 */
                             {
                                 result = lexicographicallyCompare(path[nowPath][j], path[nowPath][(j+1)%m], i);
                                 if(result == 1) /* sum2 is lexicographically smallest */
                                 {
-                                    for(k=0;k<i;k++)
-                                    {
-                                        path[nextPath][j][k] = path[nowPath][j][k];
-                                    }
-                                    path[nextPath][j][i] = j;
-                                    sumPath[j][i] = sum2;
+                                    cPath = 2;
                                 }
                                 else if(result == 2) /* sum3 is lexicographically smallest */
                                 {
-                                    for(k=0;k<i;k++)
-                                    {
-                                        path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                                    }
-                                    path[nextPath][j][i] = j;
-                                    sumPath[j][i] = sum3;
+                                    cPath = 3;
                                 }
                                 else /* both sum2 and sum3 is the lexicographically smallest */
                                 {
-                                    for(k=0;k<i;k++)
-                                    {
-                                        path[nextPath][j][k] = path[nowPath][j][k];
-                                    }
-                                    path[nextPath][j][i] = j;
-                                    sumPath[j][i] = sum2;
+                                    cPath = 2;
                                 }
                             }
                             else /* sum2 is lexicographically smallest */
                             {
-                                for(k=0;k<i;k++)
-                                {
-                                    path[nextPath][j][k] = path[nowPath][j][k];
-                                }
-                                path[nextPath][j][i] = j;
-                                sumPath[j][i] = sum2;
+                                cPath = 2;
                             }
                         }
                         else /* sum1 and sum2 is lexicographically equal */
@@ -326,24 +287,41 @@ int main()
                             result = lexicographicallyCompare(path[nowPath][(j+m-1)%m], path[nowPath][(j+1)%m], i);
                             if(result == 1) /* both sum1 and sum2 is the lexicographically smallest */
                             {
-                                for(k=0;k<i;k++)
-                                {
-                                    path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
-                                }
-                                path[nextPath][j][i] = j;
-                                sumPath[j][i] = sum1;
+                                cPath = 1;
                             }
                             else /* (lexicographically) sum3 < sum1 = sum2 or sum1 = sum2 = sum3 */
                             {
-                                for(k=0;k<i;k++)
-                                {
-                                    path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
-                                }
-                                path[nextPath][j][i] = j;
-                                sumPath[j][i] = sum3;
+                                cPath = 3;
                             }
                         }
                     }
+                }
+                if(cPath == 1)
+                {
+                    for(k=0;k<i;k++)
+                    {
+                        path[nextPath][j][k] = path[nowPath][(j+m-1)%m][k];
+                    }
+                    path[nextPath][j][i] = j;
+                    sumPath[j][i] = sum1;
+                }
+                else if(cPath == 2)
+                {
+                    for(k=0;k<i;k++)
+                    {
+                        path[nextPath][j][k] = path[nowPath][j][k];
+                    }
+                    path[nextPath][j][i] = j;
+                    sumPath[j][i] = sum2;
+                }
+                else /* cPath == 3 */
+                {
+                    for(k=0;k<i;k++)
+                    {
+                        path[nextPath][j][k] = path[nowPath][(j+1)%m][k];
+                    }
+                    path[nextPath][j][i] = j;
+                    sumPath[j][i] = sum3;
                 }
             }
         }
@@ -372,7 +350,7 @@ int main()
             printf(" %d", path[nextPath][result][i]+1);
         }
         printf("\n");
-        printf("%d\n", sumPath[result][n-1]);
+        printf("%d\n", min);
 
     }
 
